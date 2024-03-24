89b707dcd44cf62931b3426450d564cfd66f6326
rrodriguezgon <65360106+rrodriguezgon@users.noreply.github.com>
2024-03-25 00:37:02 +0100

    Actualizar phishTankProvider.js

--- src/providers/phishTankProvider.js
+++ src/providers/phishTankProvider.js
@@ -42,6 +42,7 @@ async function processUrls() {
       };
 
       await crearAnalizedUrl(dataUrl).catch((ex) => console.error(ex));
+      console.log('creado',data.url);
     }
   } catch (ex) {
     console.error(ex);
