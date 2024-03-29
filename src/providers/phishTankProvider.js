61118df0eb5a6402b30b4c99948b0c1989598ba6
DESKTOP-S98238J\Raquel <Raquel Rodriguez Gonzalez>
2024-03-29 16:49:37 +0100

    filtrar registros a guardar de phishtank

--- src/providers/phishTankProvider.js
+++ src/providers/phishTankProvider.js
@@ -3,19 +3,28 @@ const { crearAnalizedUrl } = require("../common/managerMongo");
 const moment = require("moment");
 
 module.exports = async function run() {
-  console.log(`INICIADO PHISTANK ${moment().format('DD/MM HH:mm:ss')}`);
+  console.log(`INICIADO PHISTANK ${moment().format("DD/MM HH:mm:ss")}`);
   await processUrls();
-  console.log(`TERMINADO  PHISTANK ${moment().format('DD/MM HH:mm:ss')}`);
+  console.log(`TERMINADO  PHISTANK ${moment().format("DD/MM HH:mm:ss")}`);
 };
 
 async function processUrls() {
   try {
     const { data } = await getItemsPHISHTANK();
-    const dia = moment();
 
-    let count = 0;
-    
-    for (const item of data) {
+    let items = data.filter(
+      (item) =>
+        moment(item.verification_time).format("DD/MM/YYYY") ===
+        moment().format("DD/MM/YYYY")
+    );
+
+    console.log(
+      "Empezado - total:",
+      items.length,
+      moment().format("DD/MM HH:mm:ss")
+    );
+
+    for (const item of items) {
       const dataUrl = {
         url: {
           domain: item.url,
@@ -24,13 +33,13 @@ async function processUrls() {
         dns: item.details.length
           ? {
               country: item.details[0].country,
-              createDate: dia,
+              createDate: moment(),
             }
           : undefined,
         whoIs: item.details.length
           ? {
               asn: `${item.details[0].announcing_network} / ${item.details[0].rir}`,
-              createDate: dia,
+              createDate: moment(),
             }
           : undefined,
         externalLinks: [item.phish_detail_url],
@@ -43,11 +52,12 @@ async function processUrls() {
       };
 
       await crearAnalizedUrl(dataUrl).catch((ex) => console.error(ex));
-      count++;
-      console.log('total:', data.length, moment().format('DD/MM HH:mm'));
-      console.log('creados:', count);
-      console.log('creado',dataUrl.url.domain);
     }
+    console.log(
+      "Terminado - total:",
+      items.length,
+      moment().format("DD/MM HH:mm:ss")
+    );
   } catch (ex) {
     console.error(ex);
   }
